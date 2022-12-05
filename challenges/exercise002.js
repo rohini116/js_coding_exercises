0efd452a5fb111cf89ac0751e72bb25927066a5e
Muthuraj Mohan kumar <mrohini116@gmail.com>
2022-12-05 14:36:06 +0000

    implemented excercise 2

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,24 +1,38 @@
 export function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+  return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  return person.city === "Manchester" ? true : false;
 }
 
 export function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  return Math.ceil(people/40);
 }
 
 export function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  let count = 0;
+  arr.forEach((element) => {
+    if(element === "sheep")
+    {
+      count = count + 1;
+    }
+  });
+  return count;
+
 }
 
 export function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  if (
+    person.address.city === "Manchester" &&
+    person.address.postCode.substring(0, 1) === "M"
+  ) {
+    return true;
+  }
+  return false;
 }
