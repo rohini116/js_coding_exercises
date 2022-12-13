72cb46422d741e6fa0e681429a0c34de2cc83748
Muthuraj Mohan kumar <mrohini116@gmail.com>
2022-12-13 23:22:41 +0000

    completed exercise 5

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -1,36 +1,55 @@
 export const findNextNumber = (nums, n) => {
   if (nums === undefined) throw new Error("nums is required");
   if (n === undefined) throw new Error("n is required");
-  // Your code here!
+  let nextNumIdx = -1;
+  if(nums.indexOf(n) != -1)
+  {
+    nextNumIdx  = nums.indexOf(n) + 1;
+  }
+  return nums[nextNumIdx] ? nums[nextNumIdx] : null;
 };
 
 export const count1sand0s = (str) => {
   if (str === undefined) throw new Error("str is required");
-  // Your code here!
+  let noOfOnes = str.match(/1/g).length;
+  let noOfZeros = str.length - noOfOnes;
+  return {1 : noOfOnes, 0 : noOfZeros}; 
 };
 
 export const reverseNumber = (n) => {
   if (n === undefined) throw new Error("n is required");
-  // Your code here!
+  return parseInt(n.toString().split("").reverse().join(""));
 };
 
 export const sumArrays = (arrs) => {
   if (arrs === undefined) throw new Error("arrs is required");
-  // Your code here!
+  const mergedArr = [].concat(...arrs);
+  return mergedArr.reduce((a,b) => a+b);
 };
 
 export const arrShift = (arr) => {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  [arr[0], arr[arr.length - 1]] = [arr[arr.length - 1], arr[0]];
+  return arr;
 };
 
 export const findNeedle = (haystack, searchTerm) => {
   if (haystack === undefined) throw new Error("haystack is required");
   if (searchTerm === undefined) throw new Error("searchTerm is required");
-  // Your code here!
+  const arrValue = Object.values(haystack);
+  let hasSearchTerm = false;
+  arrValue.forEach((val) => {
+    if(JSON.stringify(val).toLowerCase().indexOf(searchTerm.toLowerCase()) > -1) hasSearchTerm = true;
+  });
+  return hasSearchTerm;
 };
 
 export const getWordFrequencies = (str) => {
   if (str === undefined) throw new Error("str is required");
-  // Your code here!
+  let wordFrequencies = {};
+  const newStrArr = str.replace(/[^a-zA-Z ]/g, "").split(" ");
+  newStrArr.forEach(function (word, i, arr) {
+    wordFrequencies[word.toLowerCase()] = wordFrequencies[word.toLowerCase()] ? ++wordFrequencies[word.toLowerCase()] : 1;
+  });
+  return wordFrequencies;
 };
